f2dbfc699ea33cdaa372d16375a8addc6da1e0a8
Artur Diniz Adam <artur.daz@gmail.com>
2018-04-20 06:45:43 -0300

    Segundo dia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BootstrapModule } from '../modules/bootstrap/bootstrap.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
