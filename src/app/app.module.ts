d99d5acc1a2375762e0670af01c3eec29c5f88ed
Artur Diniz Adam <artur.daz@gmail.com>
2018-04-20 07:16:20 -0300

    Busca com palavra estática

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FiltroPorTituloPipe } from './pipes/filtro-por-titulo.pipe';
 
 import { HttpClientModule } from '@angular/common/http';
 import { BootstrapModule } from '../modules/bootstrap/bootstrap.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltroPorTituloPipe
   ],
   imports: [
     BrowserModule,
