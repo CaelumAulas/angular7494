742dd32f09f3917a6ca84c3d4c33ebe09923eb8e
Artur Diniz Adam <artur.daz@gmail.com>
2018-04-20 07:15:41 -0300

    Pipe de titulo uppercase e hook para elipsat titulo

--- src/modules/bootstrap/bootstrap.module.ts
+++ src/modules/bootstrap/bootstrap.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { FotoComponent } from "./components/foto/foto.component";
 import { CardComponent } from "./components/card/card.component";
 
+import { BrowserModule } from "@angular/platform-browser";
+
 @NgModule({
   declarations: [FotoComponent, CardComponent]
   ,exports: [FotoComponent, CardComponent]
+  ,imports: [BrowserModule]
 })
 export class BootstrapModule {
 
